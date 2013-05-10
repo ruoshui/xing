c15f262515e04adbf3490e21da2c29bc90b0e9e2
ruoshui <851008988@qq.com>
2013-05-10 17:53:13 +0800

    mm

--- xing/src/cn/shui/express/scan/common/reflex.java
+++ xing/src/cn/shui/express/scan/common/reflex.java
@@ -12,7 +12,7 @@ public class reflex {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
-		GpsInfo bean = new GpsInfo();
+		VehicleMechine bean = new VehicleMechine();
 		Class<? extends Object> c = bean.getClass(); // 获得实体类
 		Field[] fs = c.getFields(); // 获得实体类属性数组
 		// 循环遍历属性数组
@@ -27,6 +27,7 @@ public class reflex {
 			//org.apache.axis2.databinding.types.soapencoding.DateTime.
 			//System.out.println(f.getName());
 		}
+		System.out.println(fs.length);
 	}
 
 }
