9e567dfb1c01553b7fcadaa9508f7d0e99a2b8a7
ruoshui <851008988@qq.com>
2013-06-05 17:56:35 +0800

    初始导入

--- xing/src/cn/shui/express/scan/DecodeHandler.java
+++ xing/src/cn/shui/express/scan/DecodeHandler.java
@@ -25,6 +25,8 @@ import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 
+import cn.shui.express.scan.utils.MyLog;
+
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.DecodeHintType;
 import com.google.zxing.LuminanceSource;
@@ -36,94 +38,104 @@ import com.google.zxing.common.HybridBinarizer;
 
 final class DecodeHandler extends Handler {
 
-  private static final String TAG = DecodeHandler.class.getSimpleName();
+	private static final String TAG = DecodeHandler.class.getSimpleName();
 
-  private final CaptureActivity activity;
-  private final MultiFormatReader multiFormatReader;
-  private boolean running = true;
+	private final CaptureActivity activity;
+	private final MultiFormatReader multiFormatReader;
+	private boolean running = true;
 
-  DecodeHandler(CaptureActivity activity, Map<DecodeHintType,Object> hints) {
-    multiFormatReader = new MultiFormatReader();
-    multiFormatReader.setHints(hints);
-    this.activity = activity;
-  }
+	DecodeHandler(CaptureActivity activity, Map<DecodeHintType, Object> hints) {
+		multiFormatReader = new MultiFormatReader();
+		multiFormatReader.setHints(hints);
+		this.activity = activity;
+	}
 
-  @Override
-  public void handleMessage(Message message) {
-    if (!running) {
-      return;
-    }
-    switch (message.what) {
-      case R.id.decode:
-        decode((byte[]) message.obj, message.arg1, message.arg2);
-        break;
-      case R.id.quit:
-        running = false;
-        Looper.myLooper().quit();
-        break;
-    }
-  }
+	@Override
+	public void handleMessage(Message message) {
+		if (!running) {
+			return;
+		}
+		switch (message.what) {
+		case R.id.decode:
+			decode((byte[]) message.obj, message.arg1, message.arg2);
+			break;
+		case R.id.quit:
+			running = false;
+			Looper.myLooper().quit();
+			break;
+		}
+	}
 
-  /**
-   * Decode the data within the viewfinder rectangle, and time how long it took. For efficiency,
-   * reuse the same reader objects from one decode to the next.
-   *
-   * @param data   The YUV preview frame.
-   * @param width  The width of the preview frame.
-   * @param height The height of the preview frame.
-   */
-  private void decode(byte[] data, int width, int height) {
-    long start = System.currentTimeMillis();
-    Result rawResult = null;
-    byte[] rotatedData = new byte[data.length];
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++)
-        rotatedData[x * height + height - y - 1] = data[x + y * width];
-    }
-    int tmp = width; // Here we are swapping, that's the difference to #11
-    width = height;
-    height = tmp;
+	/**
+	 * Decode the data within the viewfinder rectangle, and time how long it
+	 * took. For efficiency, reuse the same reader objects from one decode to
+	 * the next.
+	 * 
+	 * @param data
+	 *            The YUV preview frame.
+	 * @param width
+	 *            The width of the preview frame.
+	 * @param height
+	 *            The height of the preview frame.
+	 */
+	private void decode(byte[] data, int width, int height) {
+		long start = System.currentTimeMillis();
+		Result rawResult = null;
+		byte[] rotatedData = new byte[data.length];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++)
+				rotatedData[x * height + height - y - 1] = data[x + y * width];
+		}
+		int tmp = width; // Here we are swapping, that's the difference to #11
+		width = height;
+		height = tmp;
 
-    data = rotatedData;
-    PlanarYUVLuminanceSource source = activity.getCameraManager().buildLuminanceSource(data, width, height);
-    if (source != null) {
-      BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
-      try {
-        rawResult = multiFormatReader.decodeWithState(bitmap);
-      } catch (ReaderException re) {
-        // continue
-      } finally {
-        multiFormatReader.reset();
-      }
-    }
+		data = rotatedData;
+		PlanarYUVLuminanceSource source = activity.getCameraManager()
+				.buildLuminanceSource(data, width, height);
+		if (source != null) {
+			BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+			try {
+				rawResult = multiFormatReader.decodeWithState(bitmap);
+			} catch (ReaderException re) {
+				// continue
+			} finally {
+				multiFormatReader.reset();
+			}
+		}
 
-    Handler handler = activity.getHandler();
-    if (rawResult != null) {
-      // Don't log the barcode contents for security.
-      long end = System.currentTimeMillis();
-      Log.d(TAG, "Found barcode in " + (end - start) + " ms");
-      if (handler != null) {
-        Message message = Message.obtain(handler, R.id.decode_succeeded, rawResult);
-        Bundle bundle = new Bundle();
-        Bitmap grayscaleBitmap = toBitmap(source, source.renderCroppedGreyscaleBitmap());
-        bundle.putParcelable(DecodeThread.BARCODE_BITMAP, grayscaleBitmap);
-        message.setData(bundle);
-        message.sendToTarget();
-      }
-    } else {
-      if (handler != null) {
-        Message message = Message.obtain(handler, R.id.decode_failed);
-        message.sendToTarget();
-      }
-    }
-  }
+		Handler handler = activity.getHandler();
+		if (rawResult != null) {
+			System.out.println("获取到的值为：" + rawResult);
+			MyLog.e(TAG, "获取到的值为：" + rawResult);
+			long end = System.currentTimeMillis();
+			Log.d(TAG, "Found barcode in " + (end - start) + " ms");
+			if (handler != null) {
+				Message message = Message.obtain(handler,
+						R.id.decode_succeeded, rawResult);
+				Bundle bundle = new Bundle();
+				Bitmap grayscaleBitmap = toBitmap(source,
+						source.renderCroppedGreyscaleBitmap());
+				bundle.putParcelable(DecodeThread.BARCODE_BITMAP,
+						grayscaleBitmap);
+				message.setData(bundle);
+				message.sendToTarget();
+			}
+		} else {
+			if (handler != null) {
+				Message message = Message.obtain(handler, R.id.decode_failed);
+				message.sendToTarget();
+			}
+		}
+	}
 
-  private static Bitmap toBitmap(LuminanceSource source, int[] pixels) {
-    int width = source.getWidth();
-    int height = source.getHeight();
-    Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-    bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
-    return bitmap;
-  }
+	private static Bitmap toBitmap(LuminanceSource source, int[] pixels) {
+		int width = source.getWidth();
+		int height = source.getHeight();
+		Bitmap bitmap = Bitmap.createBitmap(width, height,
+				Bitmap.Config.ARGB_8888);
+		bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+		return bitmap;
+	}
 
 }
--- xing/src/cn/shui/express/scan/DecodeThread.java
+++ xing/src/cn/shui/express/scan/DecodeThread.java
@@ -33,65 +33,68 @@ import com.google.zxing.ResultPointCallback;
 
 /**
  * This thread does all the heavy lifting of decoding the images.
- *
+ * 
  * @author dswitkin@google.com (Daniel Switkin)
  */
 final class DecodeThread extends Thread {
 
-  public static final String BARCODE_BITMAP = "barcode_bitmap";
+	public static final String BARCODE_BITMAP = "barcode_bitmap";
 
-  private final CaptureActivity activity;
-  private final Map<DecodeHintType,Object> hints;
-  private Handler handler;
-  private final CountDownLatch handlerInitLatch;
+	private final CaptureActivity activity;
+	private final Map<DecodeHintType, Object> hints;
+	private Handler handler;
+	private final CountDownLatch handlerInitLatch;
 
-  DecodeThread(CaptureActivity activity,
-               Collection<BarcodeFormat> decodeFormats,
-               String characterSet,
-               ResultPointCallback resultPointCallback) {
+	DecodeThread(CaptureActivity activity,
+			Collection<BarcodeFormat> decodeFormats, String characterSet,
+			ResultPointCallback resultPointCallback) {
 
-    this.activity = activity;
-    handlerInitLatch = new CountDownLatch(1);
+		this.activity = activity;
+		handlerInitLatch = new CountDownLatch(1);
 
-    hints = new EnumMap<DecodeHintType,Object>(DecodeHintType.class);
+		hints = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
 
-    // The prefs can't change while the thread is running, so pick them up once here.
-    if (decodeFormats == null || decodeFormats.isEmpty()) {
-      SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
-      decodeFormats = EnumSet.noneOf(BarcodeFormat.class);
-      if (prefs.getBoolean(PreferencesActivity.KEY_DECODE_1D, false)) {
-        decodeFormats.addAll(DecodeFormatManager.ONE_D_FORMATS);
-      }
-      if (prefs.getBoolean(PreferencesActivity.KEY_DECODE_QR, false)) {
-        decodeFormats.addAll(DecodeFormatManager.QR_CODE_FORMATS);
-      }
-      if (prefs.getBoolean(PreferencesActivity.KEY_DECODE_DATA_MATRIX, false)) {
-        decodeFormats.addAll(DecodeFormatManager.DATA_MATRIX_FORMATS);
-      }
-    }
-    hints.put(DecodeHintType.POSSIBLE_FORMATS, decodeFormats);
+		// The prefs can't change while the thread is running, so pick them up
+		// once here.
+		if (decodeFormats == null || decodeFormats.isEmpty()) {
+			SharedPreferences prefs = PreferenceManager
+					.getDefaultSharedPreferences(activity);
+			decodeFormats = EnumSet.noneOf(BarcodeFormat.class);
+			if (prefs.getBoolean(PreferencesActivity.KEY_DECODE_1D, false)) {
+				decodeFormats.addAll(DecodeFormatManager.ONE_D_FORMATS);
+			}
+			if (prefs.getBoolean(PreferencesActivity.KEY_DECODE_QR, false)) {
+				decodeFormats.addAll(DecodeFormatManager.QR_CODE_FORMATS);
+			}
+			if (prefs.getBoolean(PreferencesActivity.KEY_DECODE_DATA_MATRIX,
+					false)) {
+				decodeFormats.addAll(DecodeFormatManager.DATA_MATRIX_FORMATS);
+			}
+		}
+		hints.put(DecodeHintType.POSSIBLE_FORMATS, decodeFormats);
 
-    if (characterSet != null) {
-      hints.put(DecodeHintType.CHARACTER_SET, characterSet);
-    }
-    hints.put(DecodeHintType.NEED_RESULT_POINT_CALLBACK, resultPointCallback);
-  }
+		if (characterSet != null) {
+			hints.put(DecodeHintType.CHARACTER_SET, characterSet);
+		}
+		hints.put(DecodeHintType.NEED_RESULT_POINT_CALLBACK,
+				resultPointCallback);
+	}
 
-  Handler getHandler() {
-    try {
-      handlerInitLatch.await();
-    } catch (InterruptedException ie) {
-      // continue?
-    }
-    return handler;
-  }
-
-  @Override
-  public void run() {
-    Looper.prepare();
-    handler = new DecodeHandler(activity, hints);
-    handlerInitLatch.countDown();
-    Looper.loop();
-  }
-
+	Handler getHandler() {
+		try {
+			handlerInitLatch.await();
+		} catch (InterruptedException ie) {
+			// continue?
+		}
+		return handler;
+	}
+	
+	@Override
+	public void run() {
+		Looper.prepare();
+		handler = new DecodeHandler(activity, hints);
+		handlerInitLatch.countDown();
+		Looper.loop();
+	}
+	
 }
